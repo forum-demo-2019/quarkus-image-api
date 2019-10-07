079d2ee8d1ef1fd564ff7f4e2c6f97d9eadf0858
Maxim Burgerhout <maxim@redhat.com>
2019-10-07 14:14:35 +0200

    Remove unused imports

--- src/main/java/com/wzzrd/rest/json/ImageResource.java
+++ src/main/java/com/wzzrd/rest/json/ImageResource.java
@@ -1,14 +1,10 @@
 package com.wzzrd.rest.json;
 
-import javax.json.bind.Jsonb;
-import javax.json.bind.JsonbBuilder;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.PathParam;
 
 import java.io.IOException;
