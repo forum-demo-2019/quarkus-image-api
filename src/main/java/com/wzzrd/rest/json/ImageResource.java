06f96f76c712229b0acee057f6a9020eefebe6c8
Maxim Burgerhout <maxim@redhat.com>
2019-10-07 13:15:54 +0200

    Semi getting lists

--- src/main/java/com/wzzrd/rest/json/ImageResource.java
+++ src/main/java/com/wzzrd/rest/json/ImageResource.java
@@ -1,14 +1,20 @@
 package com.wzzrd.rest.json;
 
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.stream.Stream;
+import java.util.stream.*;
+import java.util.*;
 
 @Path("/api")
 public class ImageResource {
@@ -19,12 +25,18 @@ public class ImageResource {
     public String all_images() throws IOException{
         String dirName = "/home/mburgerh/work/java/quarkus-img-api/src/main/resources/images";
         try (Stream<java.nio.file.Path> paths = Files.walk(Paths.get(dirName))) {
-            paths.filter(Files::isRegularFile)
-                    .forEach(System.out::println);
+            System.out.println(paths);
+            List<java.nio.file.Path> imageList = paths.collect(Collectors.toList());
+            System.out.println(imageList);
+            ArrayList<java.nio.file.Path> imageArrayList = new ArrayList<java.nio.file.Path>(imageList);
+            System.out.println(imageArrayList);
+            return "foo";
+
         } catch (IOException ex) {
                 System.out.println("problem! image can't be loaded!");
         }
-        return "all_images";
+        return "bar";
+
     }
 
     @GET
